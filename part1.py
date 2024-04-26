83121f741775b3bdf53ce0967f382f306a4838a8
StevenChiu2018 <sam.200846@gmail.com>
2024-04-25 19:48:46 -0700

    Part 1 finished

--- part1.py
+++ part1.py
@@ -3,9 +3,38 @@ from env import ENV
 
 ENV.load_env()
 
-feader_a = Feader(name='Feader A', position=Position(10, 40), discover_possibility=ENV.get('ALPHA_A'),
+# time_steps_list = []
+
+# for round in range(50):
+#     print(f'{round} round', end='\r')
+#     feader_a = Feader(name='Feader_A', position=Position(10, 40), discover_possibility=ENV.get('ALPHA_A'),
+#                       attrition_possibility=ENV.get('LAMBDA_A'), recuitment_possibility=ENV.get('BETA_A'), unit=100)
+#     feader_b = Feader(name='Feader_B', position=Position(40, 40), discover_possibility=ENV.get('ALPHA_B'),
+#                       attrition_possibility=ENV.get('LAMBDA_B'), recuitment_possibility=ENV.get('BETA_B'), unit=100)
+#     feaders = [feader_a, feader_b]
+
+#     nest = Nest(position=Position(25, 5))
+#     nature = Nature(feaders=feaders, nest=nest)
+
+#     ants = [Ant(initial_position=Position(25, 5), nature=nature)
+#             for _ in range(100)]
+
+#     abm = ABM(ants=ants, feaders=feaders, nest=nest, need_report=False)
+
+#     time_steps = abm.execute()
+#     time_steps_list.append(time_steps)
+
+# print("")
+# print("restriction")
+# print(sum(time_steps_list) / 50)
+# print(max(time_steps_list))
+# print(min(time_steps_list))
+# print(abm.statistic.first_commit)
+
+
+feader_a = Feader(name='Feader_A', position=Position(10, 40), discover_possibility=ENV.get('ALPHA_A'),
                   attrition_possibility=ENV.get('LAMBDA_A'), recuitment_possibility=ENV.get('BETA_A'), unit=100)
-feader_b = Feader(name='Feader B', position=Position(40, 40), discover_possibility=ENV.get('ALPHA_B'),
+feader_b = Feader(name='Feader_B', position=Position(40, 40), discover_possibility=ENV.get('ALPHA_B'),
                   attrition_possibility=ENV.get('LAMBDA_B'), recuitment_possibility=ENV.get('BETA_B'), unit=100)
 feaders = [feader_a, feader_b]
 
@@ -15,20 +44,8 @@ nature = Nature(feaders=feaders, nest=nest)
 ants = [Ant(initial_position=Position(25, 5), nature=nature)
         for _ in range(100)]
 
-abm = ABM(ants=ants, feaders=feaders, nest=nest, need_report=True)
+abm = ABM(ants=ants, feaders=feaders, nest=nest, need_report=False)
 
-result = abm.execute()
-
-uncommited = 0
-commit_a = 0
-commit_b = 0
-for ant in ants:
-    if ant.state == Status.UNCOMMITTED:
-        uncommited += 1
-    elif ant.committed_to == feader_a:
-        commit_a += 1
-    else:
-        commit_b += 1
-
+abm.execute()
 
 abm.statistic.draw_status_line_graph()
